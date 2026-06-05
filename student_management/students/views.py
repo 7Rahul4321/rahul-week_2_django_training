d47cc564b526044c6d677378543770be5aca2a62
Rahul Bishnoi <bishnoirahul107@gmail.com>
2026-06-05 11:49:12 +0530

    added django templates and frontend page

--- student_management/students/views.py
+++ student_management/students/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to Student Management System")
+    return render(request, 'students/home.html')
