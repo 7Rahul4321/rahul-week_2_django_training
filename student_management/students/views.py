8cc2b624a1196ad073c4b965b6a707ceed2964e5
Rahul Bishnoi <bishnoirahul107@gmail.com>
2026-06-05 12:16:10 +0530

    completed django templates and frontend ui

--- student_management/students/views.py
+++ student_management/students/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'students/home.html')
+
+    students = [
+        {"name": "Rahul", "branch": "CSE", "year": "2nd"},
+        {"name": "Aman", "branch": "AI/ML", "year": "3rd"},
+        {"name": "Priya", "branch": "IT", "year": "2nd"},
+        {"name": "Rohit", "branch": "Data Science", "year": "1st"}
+    ]
+
+    return render(request, 'students/home.html', {'students': students})
