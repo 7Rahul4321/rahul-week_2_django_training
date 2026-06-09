063acd1502761fc2fb1c42f6c406dc25baf39259
Rahul Bishnoi <bishnoirahul107@gmail.com>
2026-06-09 14:45:11 +0530

    completed day 5 database to frontend integration

--- student_management/students/views.py
+++ student_management/students/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
+from .models import Student
 
 def home(request):
-
-    students = [
-        {"name": "Rahul", "branch": "CSE", "year": "2nd"},
-        {"name": "Aman", "branch": "AI/ML", "year": "3rd"},
-        {"name": "Priya", "branch": "IT", "year": "2nd"},
-        {"name": "Rohit", "branch": "Data Science", "year": "1st"}
-    ]
+    students = Student.objects.all()
 
     return render(request, 'students/home.html', {'students': students})
