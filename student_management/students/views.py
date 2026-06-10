aaf8f5650f85aa3d3f7f15eafaee5449b4b5cba6
Rahul Bishnoi <bishnoirahul107@gmail.com>
2026-06-10 12:40:03 +0530

    added student creation form

--- student_management/students/views.py
+++ student_management/students/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
 
 def home(request):
+
+    if request.method == "POST":
+        name = request.POST['name']
+        branch = request.POST['branch']
+        year = request.POST['year']
+
+        Student.objects.create(
+            name=name,
+            branch=branch,
+            year=year
+        )
+
+        return redirect('/')
+
     students = Student.objects.all()
 
     return render(request, 'students/home.html', {'students': students})
